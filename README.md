5124d339a777636c5478e4a5e22114562b9b1727
RafalBerezin <rafalberezin+github@gmail.com>
2024-06-02 13:22:44 +0200

    feat: add the script

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pin Youtube Comment to Screen
+
+A simple [tampermonkey](https://www.tampermonkey.net/) script to keep a youtube comment on top of the window.
+
+---
+
+Controls:
+
+- Press <kbd>P</kbd> while hovering over a comment to pin it. Press again to unpin.
+- Press <kbd>H</kbd> to hide/show the pinned comment.
---
+++ pin_youtube_comment_to_screen.js
@@ -0,0 +1,89 @@
+// ==UserScript==
+// @name         Pin Youtube Comment To Screen
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Pin Youtube Comment To Screen
+// @author       RafalBerezin
+// @match        https://www.youtube.com/watch?*
+// ==/UserScript==
+
+(function () {
+
+    // CONFIG
+
+    // No alternative way for now. Maybe I'll add buttons to the comments in the future.
+    const useKeybinds = true;
+    const pinKeybind = "p";
+    const hideKeybind = "h";
+
+    const pinnedClass = "pinnedComment";
+    const hiddenClass = "hidePin";
+
+    const style = `
+        .${pinnedClass}:not(.${hiddenClass}) {
+            position: fixed !important;
+            right: 0;
+            top: 0;
+            max-width: 80ch !important;
+            max-height: 100vh !important;
+            padding: 1em;
+            background-color: #000000cc;
+            border: 1px solid white;
+            overflow-y: scroll  !important;
+            box-sizing: border-box;
+            z-index: 2200;
+            & #content-text.ytd-comment-view-model {
+                color: white;
+            }
+        }
+    `;
+
+    // CODE
+
+    let pinnedComment = null;
+    let hoveredComment = null;
+    const commentNodeSelector = "ytd-comment-thread-renderer";
+
+    function togglePin() {
+        if (pinnedComment) unpin();
+        else pin();
+    }
+
+    function pin() {
+        if (!hoveredComment) return;
+        pinnedComment = hoveredComment;
+        pinnedComment.classList.add(pinnedClass);
+    }
+
+    function unpin() {
+        if (!pinnedComment) return;
+        pinnedComment.classList.remove(pinnedClass);
+        pinnedComment.classList.remove(hiddenClass);
+        pinnedComment = null;
+    }
+
+    function getCommentNode(element) {
+        return element.matches(commentNodeSelector) ? element : element.closest(commentNodeSelector);
+    }
+
+    function toggleHide() {
+        if (pinnedComment) pinnedComment.classList.toggle(hiddenClass);
+    }
+
+    if (useKeybinds) {
+        window.addEventListener('mouseover', e => {
+            hoveredComment = getCommentNode(e.target);
+        });
+
+        window.addEventListener('keydown', e => {
+            const key = e.key.toLowerCase();
+
+            if (key === pinKeybind) return togglePin();
+            if (key === hideKeybind) return toggleHide();
+        });
+    }
+
+    document.head.appendChild(document.createElement("style")).innerHTML = style;
+
+    console.log("[Pin Comment] Script Initialized");
+})();
